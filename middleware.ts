5cd1e114bacb3e05b76d31c97f45d801a0974824
Suggus1899 <gustavojose0819@gmail.com>
2026-06-28 20:11:12 -0400

    refactor: migrate authentication middleware logic to proxy module

--- middleware.ts
+++
@@ -1,6 +0,0 @@
-export { auth as middleware } from "@/auth"
-
-// Configura las rutas en las que el middleware actuará (solo proteger el panel /admin)
-export const config = {
-  matcher: ["/admin/:path*"],
-}
